9db7e23b6bef3ec3280cca8b6c665e10be9fa22c
Daniel Alemayehu <danialemayehu32@gmail.com>
2023-06-05 21:37:13 +0300

    employee login and change password

--- JavaProject/classes/Employee.java
+++ JavaProject/classes/Employee.java
@@ -1,15 +1,18 @@
  // Employee.java
  import java.util.Date;
  import java.sql.*;
+ import java.util.Scanner;
  public class Employee {
     private int employeeId;
     private String name;
     private String email;
     private String department;
     private Date dateOfJoining;
-    private int roleId;
-    private DatabaseConnection dbConnection; 
+    private static DatabaseConnection dbConnection; 
 
+    public Employee(String email) {
+        this.email = email;
+    }
 
     public Employee(int employeeId, String name, String email, String department, Date dateOfJoining, int roleId) {
         this.employeeId = employeeId;
@@ -17,7 +20,6 @@
         this.email = email;
         this.department = department;
         this.dateOfJoining = new Date();
-        this.roleId = roleId;
         this.dbConnection = new DatabaseConnection(); 
     }
 
@@ -42,10 +44,6 @@
         return dateOfJoining;
     }
 
-    public int getRoleId() {
-        return roleId;
-    }
-
     // Setters
     public void setEmployeeId(int employeeId) {
         this.employeeId = employeeId;
@@ -67,10 +65,70 @@
         this.dateOfJoining = dateOfJoining;
     }
 
-    public void setRoleId(int roleId) {
-        this.roleId = roleId;
+    public static Employee login() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String email = scanner.nextLine();
+            String password = scanner.nextLine();
+
+            // Validate the email and password
+            if (validateLogin(email, password)) {
+                System.out.println("Login successful.");
+                return new Employee(email); // backup to the getData class - using instance to get the email
+            } else {
+                System.out.println("Invalid email or password.");
+                return null;
+            }
+        }
     }
         
+    private static boolean validateLogin(String email, String password) {
+        try {
+            Connection connection = dbConnection.getConnection();
+            String sql = "SELECT COUNT(*) FROM employee_password WHERE email = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, email);
+            statement.setString(2, password);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                int count = resultSet.getInt(1);
+                return count > 0; // Return true if at least one matching record is found
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false; // Return false if an error occurred or no matching record found
+    }
+
+    public void changePassword(String newPassword) {
+        //use the getData class to get the email
+        try {
+            Connection connection = dbConnection.getConnection();
+            String sql = "UPDATE employee_password SET password = ? WHERE email = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, newPassword);
+            statement.setString(2, email);
+
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Password changed successfully.");
+            } else {
+                System.out.println("Failed to change password.");
+            }
+
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("An error occurred while changing the password.");
+        }
+    }
+
     public void markAttendance(Date date, String status, String remarks) {
 
         // Open database connection
